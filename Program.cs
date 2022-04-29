21833530424a7448b3d67e704f7c7f2e6d4c3589
yyogesh <yogeshggn1@gmail.com>
2022-04-29 12:52:27 +0530

    configure connection string

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using webapi_core_project.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,13 +10,21 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnectionString");
+builder.Services.AddDbContext<ProductDbContext>(options =>
+    options.UseSqlite(connectionString));
+
 var app = builder.Build();
+
+ProductDbContext dbContext = app.Services.GetRequiredService<ProductDbContext>();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    dbContext.Database.EnsureCreated();
 }
 
 app.UseHttpsRedirection();
@@ -23,3 +34,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+dbContext.Seed();
